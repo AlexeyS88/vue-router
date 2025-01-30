6c8d422eb13a9ea30879089f136e33fd7657a968
AlexeyS88 <antey343@gmail.com>
2025-01-30 21:21:45 +0300

    Add import mail

--- src/router.js
+++ src/router.js
@@ -2,9 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from "@/views/Login.vue";
 import Forget from "@/views/Forget.vue";
 import Dashboard from "@/views/Dashboard.vue";
-import Mail from "@/views/Mail.vue";
+// import Mail from "@/views/Mail.vue";
 import AppEmailBody from "@/components/AppEmailBody.vue";
 import NotFound from "@/views/NotFound.vue";
+
+const Mail = () => import('@/views/Mail.vue')
 
 const router = createRouter({
     history: createWebHistory(),
@@ -18,7 +20,7 @@ const router = createRouter({
             beforeEnter() {
                 console.log('beforeEnter')
             }},
-        { path: '/mail', component: Mail, children: [
+        { path: '/mail', component: Mail, name: 'email', children: [
                 { path: ':mailId?', component: AppEmailBody, props: true }
             ]},
         { path: '/:notFound(.*)', component: NotFound },
